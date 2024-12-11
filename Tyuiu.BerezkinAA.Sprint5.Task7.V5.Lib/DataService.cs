6dd01589cd8f813887d74acb91933d997b129fe1
Berezkin Artem <berezkin.artjom@yandex.ru>
2024-12-12 02:32:34 +0500

    Add project files.

---
+++ Tyuiu.BerezkinAA.Sprint5.Task7.V5.Lib/DataService.cs
@@ -0,0 +1,7 @@
+﻿namespace Tyuiu.BerezkinAA.Sprint5.Task7.V5.Lib
+{
+    public class DataService
+    {
+
+    }
+}
---
+++ Tyuiu.BerezkinAA.Sprint5.Task7.V5/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tyuiu.BerezkinAA.Sprint5.Task7.V5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
